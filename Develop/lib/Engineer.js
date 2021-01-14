df427972b88a1df79b8d8aa4364fcc3335ba5800
Juno Brown <amarbrown@gmail.com>
2021-01-13 20:57:50 -0500

    First test passes, imported all npm files

---
+++ Develop/lib/Engineer.js
@@ -0,0 +1,13 @@
+const Employee = require("./Employee");
+
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer extends Employee {
+    constructor(github) {
+        this.github = github;
+
+    };
+    getRole() {
+        return this.github;
+    }
+    
+};
