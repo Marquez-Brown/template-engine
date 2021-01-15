f7c7694e17a75aafde7d9179180f7569629d5fe4
Juno Brown <amarbrown@gmail.com>
2021-01-14 20:09:30 -0500

    created new function and fixed scoping issues

--- Develop/app.js
+++ Develop/app.js
@@ -4,29 +4,57 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+// const util = require('util');
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+// const writeFileAsync = util.promisify(fs.writeFile);
 const render = require("./lib/htmlRenderer");
 
-
+const employeeArray = []
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-const promptUser = () => 
-  inquirer.prompt([
+const createManager = () => {
+inquirer.prompt ([
     {
-      type: 'confirm',
-      message: 'Would you like to add a team member today?',
-      name: 'Boolean',
-      when: (answers) => answers.boolean === false
-    },
+        type: 'input',
+        message: 'Name',
+        name: 'name',
+        
+      },
+      {
+        type: 'input',
+        message: 'ID',
+        name: 'ID',
+          
+        },
+      {
+          type: 'input',
+          message: 'Email',
+          name: 'email',
+          
+        },
+        {
+          type: 'input',
+          message: 'Enter your team members office number',
+          name: 'officeNumber',
+          
+          
+        },
+]).then (answers => {
+    const manager = new Manager (answers.name, answers.ID, answers.email, answers.officeNumber) ;
+    employeeArray.push(manager);
+    choices ();
+})
+}
+const choices = () => {
+  inquirer.prompt([
     {
       type: 'list',
       message: 'What is the role of the employee',
       name: 'role',
-      choices: ['Manager', 'Engineer', 'Intern']
-        
+      choices: ['Engineer', 'Intern']
+    
       },
     {
       type: 'input',
@@ -40,27 +68,61 @@ const promptUser = () =>
       name: 'ID',
         
       },
-    
-    {
-      type: 'number',
-      message: 'Enter your team members office number',
-      name: 'officeNumber',
-      
-    },
     {
         type: 'input',
         message: 'Email',
-        name: 'Email',
+        name: 'email',
         
       },
-  ]);
+     
+      {
+        type: 'input',
+        message: 'Enter the Github account',
+        name: 'github',
+        when: (choices) => choices.role === 'Engineer',
+        
+      },
+      {
+        type: 'input',
+        message: 'Enter the school name',
+        name: 'school',
+        when: (choices) => choices.role === 'Intern',
+        
+      },
+      {
+        type: 'list',
+        message: 'Would you like to add a new employee?',
+        name: 'continue',
+        choices: ['Yes', 'No'],
+        
+      },
+      
+  ])
 
 
-promptUser()
-.then((answers) => writeFileAsync('outputPath', render(answers)))
-.then(() => console.log('Successfully wrote to readme.md'))
+
+.then((answers) => { 
+    let employee;
+    if (answers.role === 'Engineer') {
+        employee = new Engineer (answers.name, answers.ID, answers.email, answers.github)
+    } else {
+        employee = new Intern (answers.name, answers.ID, answers.email, answers.school)
+    }
+    employeeArray.push(employee);
+    if (answers.continue === "Yes") {
+        choices()
+    } else {
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath, render(employeeArray), 'utf-8')
+    }
+    
+})
 .catch((err) => console.error(err));
+}
 
+createManager ();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
